69b1598abbccb190218c0a98a6628e517de38403
Hmvp <hmvp@users.noreply.github.com>
2021-12-01 09:20:20 +0100

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,38 @@
+// #![allow(clippy::nonminimal_bool)]
+//#![deny(clippy::pedantic)]
+// #![allow(clippy::cast_possible_wrap)]
+// #![allow(clippy::cast_possible_truncation)]
+// #![allow(clippy::cast_precision_loss)]
+// #![allow(clippy::cast_sign_loss)]
+// #![allow(clippy::must_use_candidate)]
+// #![allow(clippy::implicit_hasher)]
+
+use aoc_runner_derive::aoc_lib;
+
+// pub mod day1;
+// pub mod day10;
+// pub mod day11;
+// pub mod day12;
+// pub mod day13;
+// pub mod day14;
+// pub mod day15;
+// pub mod day16;
+// pub mod day17;
+// pub mod day18;
+// pub mod day19;
+// pub mod day2;
+// pub mod day20;
+// pub mod day21;
+// pub mod day22;
+// pub mod day23;
+// pub mod day24;
+// pub mod day25;
+// pub mod day3;
+// pub mod day4;
+// pub mod day5;
+// pub mod day6;
+// pub mod day7;
+// pub mod day8;
+// pub mod day9;
+
+aoc_lib! { year = 2021 }
