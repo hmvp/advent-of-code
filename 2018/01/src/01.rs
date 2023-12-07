a513b63db58474abc6c25e1c42a1808328a2cdc4
Hmvp <hmvp@users.noreply.github.com>
2023-12-07 21:37:33 +0100

    Add 2018 day 1 (from old code form 2018)

---
+++ 2018/01/src/01.rs
@@ -0,0 +1,33 @@
+aoc::parts!(1, 2);
+
+
+fn parse_input(input: aoc::Input) -> Vec<i32> {
+    input
+        .lines()
+        .map(|l| l.parse().unwrap())
+        .collect::<Vec<_>>()
+}
+
+fn part_1(input: aoc::Input) -> impl ToString {
+    let freqs =  parse_input(input);
+
+    freqs.iter().sum::<i32>()
+}
+
+fn part_2(input: aoc::Input) -> impl ToString {
+    let freqs =  parse_input(input);
+
+    let mut freq = 0;
+    let mut seen = vec![];
+    for delta in freqs.iter().cycle() {
+        freq += delta;
+
+        if seen.contains(&freq) {
+            return freq;
+        }
+
+        seen.push(freq);
+    }
+    0
+}
+
